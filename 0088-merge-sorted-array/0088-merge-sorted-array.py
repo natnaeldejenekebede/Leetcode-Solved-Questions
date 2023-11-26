4224c3e8ae37f0516fbee0c4bfee6b05642b3bbd
natnaeldejenekebede <110912463+natnaeldejenekebede@users.noreply.github.com>
2023-11-26 15:10:25 +0300

    Time: 38 ms (88.95%), Space: 16.5 MB (5.99%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,13 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        temp = nums1[:m]  # Create a copy of nums1 with valid elements
-        i = j = 0  # Pointers for nums1 and nums2
-        
-        for k in range(m + n):
-            if i < m and (j >= n or temp[i] <= nums2[j]):
-                nums1[k] = temp[i]
-                i += 1
-            else:
-                nums1[k] = nums2[j]
-                j += 1
+        merged = nums1[:m] + nums2[:n]
+        merged.sort()
+        nums1[:m + n] = merged
+
+
+
+
+
+
+
+
+
+
